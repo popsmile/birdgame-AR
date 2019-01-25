e70ce2a9426f1e45a6e1efb936b3e3d3e30fdfbc
popsmile <popsmile7@gmail.com>
2019-01-26 00:33:01 +0900

    Add a presentScene method to viewDidLoad

--- BirdARGame/BirdARGame/ViewController.swift
+++ BirdARGame/BirdARGame/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let scene = SKScene(fileNamed: "Scene") {
+            sceneView.presentScene(scene)
+        }
     }
 
-
 }
-
