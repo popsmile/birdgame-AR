402f7d15c3e7ce3b3c5924666229c7e4af313ce2
popsmile <popsmile7@gmail.com>
2019-01-26 00:25:34 +0900

    Create a class Scene inherited by SKScene

---
+++ BirdARGame/BirdARGame/Scene.swift
@@ -0,0 +1,14 @@
+//
+//  Scene.swift
+//  BirdARGame
+//
+//  Created by 윤지영 on 26/01/2019.
+//  Copyright © 2019 윤지영. All rights reserved.
+//
+
+import SpriteKit
+import ARKit
+
+class Scene: SKScene {
+    
+}
